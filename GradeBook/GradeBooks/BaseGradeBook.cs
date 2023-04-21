20512f2b55ad6412a1b23c93c1ddbf95ebd7255a
Arumugam K <aruashwin04@gmail.com>
2023-04-21 11:47:02 +0530

    abstract class updated

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,8 +9,10 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    //public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
+        
         public string Name { get; set; }
         public GradeBookType Type { get; set; }
         public List<Student> Students { get; set; }
